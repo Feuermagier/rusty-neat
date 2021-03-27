6ed24db007112505285bfcd456eb127ec42e5786
Florian Seligmann <floseligmann@web.de>
2021-03-27 15:59:49 +0100

    Added activation functions and biases

---
+++ src/activation.rs
@@ -0,0 +1,35 @@
+use std::f64;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum Activation {
+  IDENTITY,
+  SIGMOID,
+  RELU
+}
+
+impl Activation {
+  pub fn function(&self) -> fn(f64) -> f64 {
+    match self {
+      Activation::IDENTITY => identity,
+      Activation::SIGMOID => sigmoid,
+      Activation::RELU => relu
+    }
+  }
+}
+
+fn identity(x: f64) -> f64 {
+  x
+}
+
+fn sigmoid(x: f64) -> f64 {
+  1.0 / (1.0 + (-x).exp())
+}
+
+fn relu(x: f64) -> f64 {
+  if x > 0.0 {
+    x
+  } else {
+    0.0
+  }
+}
